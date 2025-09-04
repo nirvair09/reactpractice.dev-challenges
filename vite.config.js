c4ffd46746d344e7a3f1c73dbc41881813745c0a
nirvair09 <chandradeep262003@gmail.com>
2025-09-04 18:38:38 +0530

    Kanban Todo app with dnd-kit, localStorage, gradient background

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/reactpractice.dev-challenges/todo-dnd-app/', // 👈 important for GitHub Pages
+})

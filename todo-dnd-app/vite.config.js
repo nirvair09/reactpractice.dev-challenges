c0dbb8d3639d29ab72fc35d4ec7359a6780441aa
nirvair09 <chandradeep262003@gmail.com>
2025-09-04 18:48:54 +0530

    "base changed"

--- todo-dnd-app/vite.config.js
+++ todo-dnd-app/vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/reactpractice.dev-challenges/todo-dnd-app/', // 👈 important for GitHub Pages
 })
